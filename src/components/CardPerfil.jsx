94f7b09ee82acf5f52903fb8421a4632f695ed88
JeffSSampaio <jeffersonsamp2012@gmail.com>
2025-12-17 17:52:04 -0300

    feat: adicionando link dos icones de linkedin e github

--- src/components/CardPerfil.jsx
+++ src/components/CardPerfil.jsx
@@ -1,6 +1,9 @@
 
 import '../styles/cardperfil.css';
 import fotoPerfil from '../assets/fotoPerfil.svg';
+import githubIcon from '../assets/github_icon.svg';
+import linkedinIcon from '../assets/linkedin_icon.svg';
+/*  import instagramIcon from '../assets/instagram_icon.svg';  */
 function CardPerfil({children}) {
   
     return (
@@ -12,6 +15,16 @@ function CardPerfil({children}) {
             </div>
             <h1 className='nome-perfil'>Jefferson Sampaio</h1>
             <h2 className='objetivo-perfil'>Desenvolvedor de Software</h2>
+
+            <div className='container-redes-perfil'> 
+                <h3>Siga-me</h3>
+                <div className='container-icons'>
+                <a href="https://github.com/JeffSSampaio"><img src={githubIcon} alt="github" srcset=""  className='icon'/> </a>
+                <a href="https://www.linkedin.com/in/jeffersonssampaio/"><img src={linkedinIcon} alt="linkedin" srcset="" className='icon'/></a>
+                </div>
+
+            </div>
+          
             
 
         </div>
