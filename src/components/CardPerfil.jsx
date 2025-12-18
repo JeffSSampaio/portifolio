c7133237e92c9092c38eab2ea0baf9ffb939109b
JeffSSampaio <jeffersonsamp2012@gmail.com>
2025-12-18 18:53:25 -0300

    fix: alterações e implementações do CardPerfil

--- src/components/CardPerfil.jsx
+++ src/components/CardPerfil.jsx
@@ -7,20 +7,20 @@ import linkedinIcon from '../assets/linkedin_icon.svg';
 function CardPerfil({children}) {
   
     return (
-        <div className="card-perfil">
+        <div className="card-perfil  p-10 md:p-16  w-full max-w-[65rem] min-h-[40rem] rounded-2xl shadow-xl flex flex-col items-center overflow-hidden">
             {children}
 
-            <div className='container-foto'>
-            <img src={fotoPerfil} alt="foto" srcset="" className='fotoPerfil' />
+            <div className='container-foto' >
+            <img src={fotoPerfil} alt="foto"  className='fotoPerfil' />
             </div>
             <h1 className='nome-perfil'>Jefferson Sampaio</h1>
             <h2 className='objetivo-perfil'>Desenvolvedor de Software</h2>
-
+          
             <div className='container-redes-perfil'> 
                 <h3>Siga-me</h3>
                 <div className='container-icons'>
-                <a href="https://github.com/JeffSSampaio"><img src={githubIcon} alt="github" srcset=""  className='icon'/> </a>
-                <a href="https://www.linkedin.com/in/jeffersonssampaio/"><img src={linkedinIcon} alt="linkedin" srcset="" className='icon'/></a>
+                <a href="https://github.com/JeffSSampaio"><img src={githubIcon} alt="github"  className='icon'/> </a>
+                <a href="https://www.linkedin.com/in/jeffersonssampaio/"><img src={linkedinIcon} alt="linkedin"  className='icon'/></a>
                 </div>
 
             </div>
