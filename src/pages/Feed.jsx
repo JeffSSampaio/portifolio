b0307abc93fc34534f32849cbed255af9c3217f5
JeffSSampaio <jeffersonsamp2012@gmail.com>
2025-12-18 23:20:46 -0300

    feat:criação e estilização da tela do feed

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,10 +1,22 @@
 import  useTitulo  from '../hooks/useTitulo';
-
+import Header from '../components/Header';
+import Rodape from '../components/Rodape';
+import Post from '../components/Post';
 function Feed() {
     useTitulo('Feed');
     return(
-        <div>
-          <h1>Pagina de Feed</h1>
+        <div className="flex flex-col min-h-screen"> 
+              <Header children={ <li><a href="/homepage">Home</a></li>}/>
+          
+          <h1 className='flex items-center justify-center m-[20px] texto text-[30px] font-bold'>Feed</h1>
+          
+          <main className='flex flex-col flex-grow justify-start items-center '>
+          <Post />
+          </main>
+
+          <Rodape>
+            <p className="text-center py-2 text-white">© 2024 Jefferson Sampaio</p>
+          </Rodape>
         </div>
     )
 }
