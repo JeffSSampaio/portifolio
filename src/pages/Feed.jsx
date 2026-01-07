9565ddb50b3247fd8345c7d359bb326da0079ed8
JeffSSampaio <jeffersonsamp2012@gmail.com>
2026-01-07 15:39:10 -0300

    fix: alteração no titulo

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -8,7 +8,7 @@ function Feed() {
         <div className="flex flex-col min-h-screen"> 
               <Header children={ <li><a href="/homepage">Home</a></li>}/>
           
-          <h1 className='flex items-center justify-center m-[20px] texto text-[30px] font-bold'>Feed</h1>
+          <h1 className='flex items-center justify-center m-[20px] texto text-[30px] font-bold'>Publicações</h1>
           
           <main className='flex flex-col flex-grow justify-start items-center '>
           <Post />
